4d5807aaf50695d7f45153ede6a213f8b026ecd8
Rafael Railton <rafaelrailton@gmail,com>
2021-08-23 10:59:54 -0400

    test de usuario / servidor

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const { json } = require("express");
 let express = require("express");
 let app = express();
 let mongoose = require("mongoose");
-
+let user = require("./models/User");
+let bcrypt = require("bcrypt");
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
@@ -11,8 +11,33 @@ mongoose.connect("mongodb://localhost:27017/guiapics",{useNewUrlParser:true,useU
 }).catch((err)=>{
     console.log(err);
 });
+let User = mongoose.model("User",user);
+
 app.get("/",(req,res) => {
     res.json({});
 });
+app.post("/user", async (req,res)=>{
+    if(req.body.name =="" || req.body.email == "" || req.body.password == ""){
+        res.sendStatus(400);
+        return;
+    }
+    try {
+         let user = await User.findOne({"email":req.body.email});
+         if(user != undefined){
+             res.statusCode = 400;
+             res.json({error: "Email já cadastrado"});
+             return;
+         }
+         let password = req.body.password;
+         let salt     = await bcrypt.genSalt(10);
+         let hash     = await bcrypt.hash(password,salt)
+         let newUser = new User({name:req.body.name, email:req.body.email , password:hash});
+         await newUser.save();
+        res.json({email:req.body.email});
+    }catch (error) {
+        console.log(error);
+       res.sendStatus(500);
+    }
 
+});
 module.exports = app;
--- test/user.test.js
+++ test/user.test.js
@@ -3,18 +3,54 @@ let supertest = require("supertest");
 let request   = supertest(app);
 
 describe("Cadastro de usuario",() => {
-    test("Deve cadastrar um usuário com sucesso",() => {
+    test("Deve cadastrar um usuário com sucesso",()=> {
         let time = Date.now();
         let email = `${time}@gmail.com`;
-        let user  = {name:"Victor", email,password:"12345"};
-
+        let user  = {name:"Victor", email:email,password:"12345"};
+       
         return request.post("/user")
         .send(user)
-        .then((res)=>{
+        .then(res =>{
             expect(res.statusCode).toEqual(200);
             expect(res.body.email).toEqual(email);
-        }).catch((error)=> {
-            fail(error);
+        }).catch(error => {
+          fail(error);
         });
     });
+
+    test("Deve impedir que um usuário se cadastre com os dados vazios",() =>{
+        let user  = {name:"", email:"",password:""};
+       
+        return request.post("/user")
+        .send(user)
+        .then(res =>{
+          expect(res.statusCode).toEqual(400); // 400 = Bad request 
+        }).catch(error => {
+          fail(error);
+        });
+    });
+
+    test("Deve impedir que o usuario se cadastre com um e-mail repetido",() => {
+        let time = Date.now();
+        let email = `${time}@gmail.com`;
+        let user  = {name:"Victor", email:email,password:"12345"};
+       
+        return request.post("/user")
+        .send(user)
+        .then(res =>{
+            expect(res.statusCode).toEqual(200);
+            expect(res.body.email).toEqual(email);
+            return request.post("/user")
+            .send(user)
+            .then(res => {
+                expect(res.statusCode).toEqual(400);
+                expect(res.body.error).toEqual("Email já cadastrado");
+            }).catch(err =>{
+                 fail(err);
+            });
+        }).catch(err => {
+                fail(err);
+        });
+    });
+
 });
